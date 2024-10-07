ab095dc795557f97b64db3f1ea32c9111501525b
DenVik <denyagarkusha2@gmail.com>
2024-10-07 21:33:04 +0200

    add pokemon

--- src/components/buttons-info-arr.js
+++ src/components/buttons-info-arr.js
@@ -1,86 +1,87 @@
 export const buttonsInfo = [
-	{
-	  name: 'bulbasaur',
-	  id: 1,
-	},
-	{
-	  name: 'ivysaur',
-	  id: 2,
-	},
-	{
-	  name: 'venusaur',
-	  id: 3,
-	},
-	{
-	  name: 'charmander',
-	  id: 4,
-	},
-	{
-	  name: 'charmeleon',
-	  id: 5,
-	},
-	{
-	  name: 'charizard',
-	  id: 6,
-	},
-	{
-	  name: 'squirtle',
-	  id: 7,
-	},
-	{
-	  name: 'wartortle',
-	  id: 8,
-	},
-	{
-	  name: 'caterpie',
-	  id: 9,
-	},
-	{
-	  name: 'metapod',
-	  id: 10,
-	},
-	{
-	  name: 'butterfree',
-	  id: 11,
-	},
-	{
-	  name: 'weedle',
-	  id: 12,
-	},
-	{
-	  name: 'kakuna',
-	  id: 13,
-	},
-	{
-	  name: 'beedrill',
-	  id: 14,
-	},
-	{
-	  name: 'pidgey',
-	  id: 15,
-	},
-	{
-	  name: 'pidgeotto',
-	  id: 16,
-	},
-	{
-	  name: 'pidgeot',
-	  id: 17,
-	},
-	{
-	  name: 'rattata',
-	  id: 18,
-	},
-	{
-	  name: 'raticate',
-	  id: 19,
-	},
-	{
-	  name: 'blastoise',
-	  id: 20,
-	},
-	{
-		name: 'pikachu',
-		id: 21,
-	  },
-  ];
+  {
+    name: 'bulbasaur',
+    id: 1,
+  },
+  {
+    name: 'ivysaur',
+    id: 2,
+  },
+  {
+    name: 'venusaur',
+    id: 3,
+  },
+  {
+    name: 'charmander',
+    id: 4,
+  },
+  {
+    name: 'charmeleon',
+    id: 5,
+  },
+  {
+    name: 'charizard',
+    id: 6,
+  },
+  {
+    name: 'squirtle',
+    id: 7,
+  },
+  {
+    name: 'wartortle',
+    id: 8,
+  },
+  {
+    name: 'caterpie',
+    id: 9,
+  },
+  {
+    name: 'metapod',
+    id: 10,
+  },
+  {
+    name: 'butterfree',
+    id: 11,
+  },
+  {
+    name: 'weedle',
+    id: 12,
+  },
+  {
+    name: 'kakuna',
+    id: 13,
+  },
+  {
+    name: 'beedrill',
+    id: 14,
+  },
+  {
+    name: 'pidgey',
+    id: 15,
+  },
+  {
+    name: 'pidgeotto',
+    id: 16,
+  },
+  {
+    name: 'pidgeot',
+    id: 17,
+  },
+  {
+    name: 'rattata',
+    id: 18,
+  },
+  {
+    name: 'raticate',
+    id: 19,
+  },
+  {
+    name: 'blastoise',
+    id: 20,
+  },
+  {
+    name: 'pikachu',
+    id: 21,
+  },
+  { name: 'ditto', id: 22 },
+];
